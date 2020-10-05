3e04abfcfc18fd7c06c2817eac9e2b6874ae1fad
akhilmant <70801901+akhilmant@users.noreply.github.com>
2020-10-05 19:05:38 +0530

    Update and rename Email.py.py to Email.py

--- Email.py
+++ Email.py
@@ -1,81 +1,24 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[1]:
-
+#WILL ONLY WORK IF YOU TURN OFF THE TWO FACTOR AUTHENTICATION!
 
 import imaplib
-
-
-# In[2]:
 
 
 M=imaplib.IMAP4_SSL("imap.gmail.com")
 
 
-# In[3]:
-
-
 import getpass
 
 
-# In[4]:
-
-
 email=getpass.getpass("email : ")
-password=getpass.getpass("passwrpd: ")
-
-
-# In[5]:
+password=getpass.getpass("password: ")
 
 
 M.login(email,password)
 
-
-# In[6]:
-
-
-M.list()
-
-
-# In[7]:
-
-
 M.select("INBOX")
-
-
-# In[8]:
-
-
 typ,data=M.search(None,'SUBJECT "This is a python test"')
-
-
-# In[9]:
-
-
-typ
-
-
-# In[10]:
-
-
-data
-
-
-# In[11]:
-
-
 email_id=data[0]
-
-
-# In[ ]:
-
-
 result,email_id=M.fetch(email_id,"(RFC882)")
 
-
-# In[ ]:
-
-
-raw_email=
 
